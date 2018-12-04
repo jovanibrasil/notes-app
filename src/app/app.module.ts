a89a9fc0b9966a5b6f5c763404a31be4674500b2
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2018-12-03 23:52:45 -0200

    Add more componentes and basic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './Navigation/navigation.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NotesComponent } from './notes/notes.component';
+import { Router, Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { 
+    path: 'notes', 
+    component: NotesComponent 
+  },
+  { 
+    path: 'feedback', 
+    component: FeedbackComponent 
+  },
+  {
+    path: '',
+    component: NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent, NavigationComponent
+    AppComponent, NavigationComponent, FeedbackComponent, NotFoundComponent, NotesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
