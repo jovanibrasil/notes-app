c25eef9e8f3726e15762836fc8e8b4e9c4327798
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2018-12-03 23:30:02 -0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './Navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, NavigationComponent
   ],
   imports: [
     BrowserModule
