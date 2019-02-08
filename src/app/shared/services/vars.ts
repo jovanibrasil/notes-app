ec3afdf156778abe26b48de0c3a881af94bdf6cc
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-02-08 03:27:19 -0200

    Change AUTH_BASE_URL to https

--- src/app/shared/services/vars.ts
+++ src/app/shared/services/vars.ts
@@ -1,4 +1,4 @@
 //export const BASE_URL = "http://localhost:8080/api";
 export const NOTES_BASE_URL = "https://notes.jovanibrasil.com/notes-api";
-export const AUTH_BASE_URL = 'http://auth.jovanibrasil.com/auth-api';
+export const AUTH_BASE_URL = 'https://auth.jovanibrasil.com/auth-api';
     
