a12db470107546198639682ee2f5c05fad427db5
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-07-14 19:58:51 -0300

    Fix prod notes and auth urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   RECAPTCHA_KEY: $ENV.RECAPTCHA_KEY,
-  NOTES_BASE_URL: "https://jb-notes-api.herokuapp.com/api",
-  AUTH_BASE_URL: "https://jb-auth-api.herokuapp.com/api"
+  NOTES_BASE_URL: "https://jb-notes-api.herokuapp.com",
+  AUTH_BASE_URL: "https://jb-auth-api.herokuapp.com"
 };
